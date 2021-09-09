7cef1378158bc086415a97a3dd1644f8dd51db07
Florent <florent.git@zeteo.me>
2021-09-09 11:18:28 +0200

    Fix

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -16,12 +16,13 @@ install_node_deps() {
     pushd "$final_path"
         ynh_use_nodejs
         ynh_exec_warn_less sudo -u $app env $ynh_node_load_PATH npm install
-        # ynh_exec_warn_less sudo -u $app env $ynh_node_load_PATH NODE_ENV=production npm run build
     popd
 }
 
 run_discordas() {
-    ynh_exec_warn_less sudo -u $app env $ynh_node_load_PATH NODE_ENV=production node build/src/discordas.js -r -u "http://localhost:$port" -c config.yaml
+    pushd "$final_path"
+        ynh_exec_warn_less sudo -u $app env $ynh_node_load_PATH NODE_ENV=production node build/src/discordas.js -r -u "http://localhost:$port" -c config.yaml
+    popd
 }
 
 setup_final_path_rights() {
